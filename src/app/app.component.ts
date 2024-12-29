1e6fcc6cf7511f33e96238ef9f09b4af8ac14690
Nazmul Kabir <nazmul.kabir.ext@holcim.com>
2024-12-29 21:59:06 +0600

    project structure done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { log } from 'node:console';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'startise-xcloud';
+  showBlueprints: boolean = false;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  toggleBlueprints() {
+    this.showBlueprints = !this.showBlueprints;
+    console.log('showBlueprints', this.showBlueprints);
+  }
 }
